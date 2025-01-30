8ec85dd502673f88db8fe00e9f517f9a33818ece
Spaido652 <sd5423394014@gmail.com>
2025-01-30 21:33:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: '꧁SPÅíDØ꧂',
   author: 'Bot',
   botName: "SPAIDO",
   botOwner: '꧁BÏG~SPÅíDØ꧂', // Your name
-  ownerNumber: '09084726896', //Your number
+  ownerNumber: '23409084726896', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
